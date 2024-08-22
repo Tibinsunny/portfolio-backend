fce987dbab127fb4b6a040341b218427405a0a62
Tibinsunny <tibinsunny95@gmail.com>
2024-08-22 23:57:58 +0530

    Debugger

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const app = require("./app");
 const connection = require("./database/connection");
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
+const port = process.env.PORT || 3000;
+
+app.listen(port, "0.0.0.0", function () {
+  console.log("Server Started on Port" + port);
 });
